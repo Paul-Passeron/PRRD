479dca3d2614d097070df8e94309466e97bc7e29
prudence3333343 <76014693+prudence3333343@users.noreply.github.com>
2025-12-08 10:21:35 +0100

    definition morris

--- src/graphs.h
+++ src/graphs.h
@@ -20,4 +20,50 @@ typedef struct tree_shape_t {
     int size;
 }tree_shape_t;
 
+
+void morris(tree t){
+  tree *current = t;
+  while (current != NULL) {
+    if (current->left == NULL) {
+        printf("%d ", current->dat);  
+        current = current->right;  
+    }
+    if (p->left == NULL) {
+      visit(p);
+      morris(p->right);  
+    }
+    else {
+        if (warp(p)) {
+            morris(p->left); 
+        }
+        else {
+            visit(p);
+            morris(p->right);
+        }
+    }
+  }
+}
+
+
+bool wrap (tree t){
+  tree *current = t;
+  tree *pred = current->left;
+   while (pred->right != NULL && pred->right != current) {
+        pred = pred->right;
+      } 
+      if (pred->right == NULL){
+      pred->right = current;
+      return true;
+      }
+      else {
+        pred->right = NULL;
+        return false;
+      }
+}
+
+void visit(tree t ){
+  printf("%d ", t->data);
+}
+
+
 #endif // GRAPHS_H
