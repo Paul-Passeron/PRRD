dec38584428f7ff15d4a84b04ff64e54621d5339
prudence.deteix <prudencedeteix@LAPTOP-I24P0QFI.localdomain>
2025-12-15 12:27:42 +0100

    add ghost graphs.h

--- src/graphs.h
+++ src/graphs.h
@@ -15,13 +15,14 @@ struct tree {
 typedef struct tree *tree_t;
 
 typedef struct tree_shape_t {
-    tree_t *cell;
-    int *lchd;
-    int *rchd;
-    int *lmdt;
-    int *rmdt;
+    tree_t *cell; //tree node
+    int *lchd; // left child
+    int *rchd; // right child
+    int *lmdt; // leftmost descendant
+    int *rmdt; // rightmost descendant
     int size;
 }tree_shape_t;
+
 
 bool warp(tree_t t, tree_t q);
 void visit(tree_t t);
@@ -30,42 +31,59 @@ void morris(tree_t t);
 
 #ifdef TREE_IMPL
 
-//@ ghost tre_shape_t morris_t;
+//@ ghost tree_shape_t morris_t;
 //@ ghost int morris_r;
 //@ ghost int morris_c;
 //@ ghost int morris_k;
+//@ ghost int  warp_j;
 
-bool warp(tree_t t, tree_t q) {
+
+bool warp(tree_t p, tree_t q) {
     if (q->right == NULL) {
-        q->right = t;
+        q->right = p;
         return true;
     }
-    if (q->right == t) {
+    if (q->right == p) {
         q->right = NULL;
         return false;
     }
-    return warp(t, q->right);
+    //@ ghost warp_j = morris_t.rchd[warp_j];
+    return warp(p, q->right);
 }
 
-void traversal(tree_t t) {
-    if (t != NULL)
-        morris(t);
+void traversal(tree_t p) {
+    if (p != NULL)
+    //@ ghost morris_t = morris_t;
+    //@ ghost morris_r = morris_k;
+    //@ ghost morris_c = 0;
+    //@ ghost morris_k = morris_k;
+        morris(p);
 }
 
-void visit(tree_t t) {
-    printf("%d ", t->dat);
+void visit(tree_t p) {
+    printf("%d ", p->dat);
 }
 
-void morris(tree_t t) {
-    if (t->left != NULL && warp(t, t->left)) {
-        morris(t->left);
+
+void morris_visit(tree_t p) {
+      //@ ghost warp_j = morris_t.lchd[morris_k];
+    if (p->left != NULL && warp(p, p->left)) {
+        //@ ghost morris_t = morris_t;
+        //@ ghost morris_r = morris_r;
+        //@ ghost morris_c = morris_c;
+        //@ ghost morris_k = morris_t.lchd[morris_k];
+        morris_visit(p->left);
     } else {
-        visit(t);
-        if (t->right != NULL)
-            morris(t->right);
+        visit(p);
+        if (p->right != NULL)
+          //@ ghost morris_t = morris_t;
+          //@ ghost morris_r = morris_r;
+          //@ ghost morris_c = morris_c + 1;
+          //@ ghost if (morris_t.lchd[morris_k] = morris_k)  {morris_k = morris_k + 1;} else {morris_k = morris_t.lchd[morris_k];}
+          morris_visit(p->right);
     }
 }
-
+                               
 
 
 #endif // TREE_IMPL
