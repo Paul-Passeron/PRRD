5dfe01df2cbf4b4e5d45cd60abda3b21b7112df5
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-15 12:11:26 +0100

    Refactor back_again requires and ghost variables
    Added necessary predicates and updated ghost variables to reflect the
    logic of the `back_again` function. This change improves the
    correctness and clarity of the ghost code.

--- src/const_space.h
+++ src/const_space.h
@@ -8,19 +8,32 @@ void tortoise_hare(lst_t bp, lst_t sp, lst_t fp, lst_t qp);
 void value_reverse(lst_t sp, lst_t qp);
 
 #ifdef CONST_SPACE_IMPL
+#include "predicates.h"
 
 //@ ghost int tortoise_hare_k;
 //@ ghost list_shape tortoise_hare_ls;
+//
 //@ ghost int back_again_k;
 //@ ghost list_shape back_again_ls;
+//
 //@ ghost list_shape value_reverse_ls;
+
 void value_reverse(lst_t sp, lst_t qp) {
   //@ ghost tortoise_hare_ls = value_reverse_ls;
   //@ ghost tortoise_hare_k = 0;
   tortoise_hare(NULL, sp, sp, qp);
 }
 
+/*@
+    // predicate reversal{L1, L2}(ls: list_shape, k: int) =
+
+ */
+
 void back_again(lst_t bp, lst_t sp, lst_t np) {
+    //@ requires listLR(back_again_ls, sp, back_again_k, back_again_ls.count, back_again_ls.cells[back_again_ls.count-1]);
+    //@ requires listRL(back_again_ls, bp, back_again_k);
+    //@ requires back_again_k <= back_again_ls.count - back_again_k;
+    //@ requires back_again_ls.cells[back_again_ls.count - back_again_k] == np;
   if (bp == NULL || np == NULL)
     return;
 
@@ -31,7 +44,7 @@ void back_again(lst_t bp, lst_t sp, lst_t np) {
   lst_t nbp = bp->cdr;
   bp->cdr = sp;
 
-  //@ ghost back_again_k = back_again_k + 1;
+  //@ ghost back_again_k = back_again_k - 1;
   //@ ghost back_again_ls = back_again_ls;
   back_again(nbp, bp, np->cdr);
 }
